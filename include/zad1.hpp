994ddcc13f2b1d0275c84b3ac86576683020094a
Michał Wysocki <01181184@pw.edu.pl>
2025-12-16 20:15:00 +0100

    :*)

--- include/zad1.hpp
+++ include/zad1.hpp
@@ -6,3 +6,15 @@
 
 // Zad1
 // tutaj definicja klasy Tagliatelle
+
+class Tagliatelle
+{
+public:
+    Tagliatelle(double dlugosc, double szerokosc, double proporcja) : L(dlugosc), W(szerokosc), R(proporcja) {}
+    Tagliatelle() : L(0.5), W(0.5), R(0.5) {}
+    double ileMaki(unsigned P) { return P * L * W * (1. - R) * C; }
+
+private:
+    double L, W, R;
+    const double C;
+};
